514b58bdbc9202096b05a9138173ce645b52d148
casanovajose <jose.casanova@nayra.coop>
2020-12-14 00:34:13 -0300

    adding validations to traj add point

--- lib/live_spat_web/live/page_live.ex
+++ lib/live_spat_web/live/page_live.ex
@@ -3,12 +3,7 @@ defmodule LiveSpatWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    traj = [
-      %{x: 10, y: 20},
-      %{x: 22, y: 30},
-      %{x: 40, y: 50},
-      %{x: 60, y: 80},
-    ]
+    traj = []
     socket =
       socket
       |> assign(point: %{x: 128, y: 128}, color: "#f00")
@@ -19,20 +14,18 @@ defmodule LiveSpatWeb.PageLive do
   end
 
   @impl true
-  def handle_event("move_live_point", %{"offsetX" => x, "offsetY" => y } = mouse_point, socket) do
+  def handle_event("move_live_point", %{"offsetX" => x, "offsetY" => y } = _mouse_point, socket) do
     socket = assign(socket, :point, %{x: x, y: y})
-    # IO.inspect(mouse_point)
     {:noreply, socket}
   end
 
   @impl true
-  def handle_event("add_traj_point", %{"offsetX" => x, "offsetY" => y } = mouse_point, socket) do
-    #IO.inspect("HEY")
+  def handle_event("add_traj_point", %{"offsetX" => x, "offsetY" => y } = _mouse_point, socket) do
     socket =
       socket
-      |> update(:traj, fn t -> t ++ [%{x: x, y: y}] end)
+      |> add_point(x, y)
       |> assign(path: traj_to_path(socket.assigns.traj))
-    # IO.inspect(mouse_point)
+
     {:noreply, socket}
   end
 
@@ -40,5 +33,20 @@ defmodule LiveSpatWeb.PageLive do
     trajectory
     |> Enum.map(fn t -> "#{t.x}, #{t.y} " end)
     |> List.to_string()
+  end
+
+  defp add_point(socket, x, y) do
+    # TODO these validations should be configurables
+    socket =
+    with true <- Enum.count(socket.assigns.traj) < 256,
+        %{x: x1, y: y1} <- List.last(socket.assigns.traj),
+        true <- abs(x1 - x) > 2 || abs(y1 - y) > 2 do
+          update(socket, :traj, fn t -> t ++ [%{x: x, y: y}] end)
+    else
+      nil -> assign(socket, traj: [%{x: x, y: y}])
+      _ -> socket
+    end
+    # IO.inspect(Enum.count(socket.assigns.traj))
+    socket
   end
 end
